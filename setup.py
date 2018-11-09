30572088a99cdfe9774a21d4264e9e4150a7df3d
Mathias Lohne <mathialo@ifi.uio.no>
2018-11-09 11:31:33 +0100

    Restructure repo

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+# Install python package
+setup(
+    name="parsnet",
+    version=0.1,
+    author="Mathias Lohne",
+    author_email="mathialo@ifi.uio.no",
+    license="MIT",
+    description="TensorFlow implementation of the constraints necessary for parseval networks",
+    url="https://github.com/?/parsnet",
+    install_requires=["tensorflow>=1.5"],
+    packages=["parsnet"],
+    zip_safe=False)
