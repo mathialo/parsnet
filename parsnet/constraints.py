72efe131a2e0706b5c2b7cac5fe07dd8b45b5676
Mathias Lohne <mathialo@ifi.uio.no>
2018-11-09 11:32:15 +0100

    Add TightFrame constraint implemented after Parseval Networks paper

--- parsnet/constraints.py
+++ parsnet/constraints.py
@@ -1,1 +1,36 @@
+from tensorflow.python.keras.constraints import Constraint
+from tensorflow.python.ops import math_ops
 
+
+class TightFrame(Constraint):
+    """
+    Parseval (tight) frame contstraint, as introduced in https://arxiv.org/abs/1704.08847
+
+    Constraints the weight matrix to be a tight frame, so that the Lipschitz
+    constant of the layer is <= 1. This increases the robustness of the network
+    to adversarial noise.
+
+    Args:
+        beta (float):   Retraction parameter.
+
+    Returns:
+        Weight matrix after applying regularizer.
+    """
+
+
+    def __init__(self, beta):
+        self.beta = beta
+
+
+    def __call__(self, w):
+        return (1 + self.beta) * w - self.beta * math_ops.matmul(
+            w,
+            math_ops.matmul(w, w, transpose_a=True))
+
+
+    def get_config(self):
+        return {'beta': self.beta}
+
+
+# Alias
+tight_frame = TightFrame
