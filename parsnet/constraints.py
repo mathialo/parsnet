93c9dc46775fa0f9f3faf9df9fc2e65a9e35fd45
Mathias Lohne <mathialo@ifi.uio.no>
2018-11-14 10:58:29 +0100

    Fix dimensions issue in matmul

--- parsnet/constraints.py
+++ parsnet/constraints.py
@@ -1,5 +1,5 @@
 from tensorflow.python.keras.constraints import Constraint
-from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import math_ops, array_ops
 
 
 class TightFrame(Constraint):
@@ -23,9 +23,25 @@ class TightFrame(Constraint):
 
 
     def __call__(self, w):
-        return (1 + self.beta) * w - self.beta * math_ops.matmul(
-            w,
-            math_ops.matmul(w, w, transpose_a=True))
+        transpose_channels = (len(w.shape) == 4)
+
+        # Move channels to the front in order to make the dimensions correct for matmul
+        if transpose_channels:
+            w_reordered = array_ops.transpose(w, perm=[3, 0, 1, 2])
+        else:
+            w_reordered = w
+
+
+        # Perform the projection
+        result_reordered = (1 + self.beta) * w_reordered - self.beta * math_ops.matmul(
+            w_reordered,
+            math_ops.matmul(w_reordered, w_reordered, transpose_a=True))
+
+        # Move channels to the back again
+        if transpose_channels:
+            return array_ops.transpose(result_reordered, perm=[1, 2, 3, 0])
+        else:
+            return result_reordered
 
 
     def get_config(self):
